7daf303b7a4a0f211e5069859ba674743b0f1f45
j7powell1 <130012325+j7powell1@users.noreply.github.com>
2023-05-24 18:21:39 -0700

    first commit

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -7,6 +7,28 @@ mkdir grading-area
 
 git clone $1 student-submission
 echo 'Finished cloning'
+if [[ -f student-submission/ListExamples.java ]]
+then
+    echo "File was found!"
+else echo "Did not find the file!"
+fi
+
+cp -r lib student-submission/ListExamples.java TestListExamples.java grading-area
+
+
+cd grading-area
+
+javac -cp "$CPATH" ListExamples.java TestListExamples.java
+
+# javac -cp $CPATH grading-area/TestListExamples.java
+
+if [[ $? -eq 0 ]]
+then    
+    echo “Compile success!”
+else
+    echo “Compile failure”
+fi
+java -cp "$CPATH" org.junit.runner.JUnitCore TestListExamples
 
 
 # Draw a picture/take notes on the directory structure that's set up after
